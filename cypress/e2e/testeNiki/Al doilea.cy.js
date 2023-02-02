086eee42c204cd531dabe95044bf6444725de8f8
Andreea Dragnea <deeamisu2004@yahoo.com>
2023-02-02 13:46:28 +0200

    initial commit

---
+++ cypress/e2e/testeNiki/Al doilea.cy.js
@@ -0,0 +1,16 @@
+describe('pe site ul Niki',()=>{
+    
+    it('ma pot loga', () => {
+
+        cy.visit('https://www.nichiduta.ro');
+        cy.get('.header-tm-logos > a').click();
+        cy.get('#acc_button').click();
+        cy.get('.account_options > .buttonBorder').click();
+        cy.get('#valid-email').type('testnichiduta@gmail.com');
+        cy.get('#valid-password').type('test1234');
+        cy.get('#submitinreg').click();
+        cy.get('.notification').should('exist');
+
+
+    })
+})
---
+++ cypress/e2e/testeNiki/PRIMUL.cy.js
@@ -0,0 +1,12 @@
+describe('Site Niki', () => {
+
+// Testul numarul 1
+ it('functioneaza cu o cautare basic' ,() =>{
+
+    cy.visit('https://www.nichiduta.ro');
+    cy.get('.header-input-search').type('carucior ').type('{enter}');
+
+    cy.get('#prod_no').should('exist');
+
+    })
+})
